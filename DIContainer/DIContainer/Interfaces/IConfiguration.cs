24c993d7853b9da8eac5af1511165bb05f93eef1
MikhailFrenkel <frenkel.misha@mail.ru>
2018-11-29 22:39:56 +0300

    added interfaces

---
+++ DIContainer/DIContainer/Interfaces/IConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIContainer.Interfaces
+{
+    public interface IConfiguration
+    {
+        void Register<TImplementation>() where TImplementation : class;
+
+        void Register<TDependency, TImplementation>()
+            where TDependency : class
+            where TImplementation : TDependency;
+    }
+}
---
+++ DIContainer/DIContainer/Interfaces/IDIContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIContainer.Interfaces
+{
+    public interface IDIContainer
+    {
+        TDependency Resolve<TDependency>() where TDependency : class;
+    }
+}
